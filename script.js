e070cadc3d6892b73e4dc54eddb57ef02a561c6e
ivoryseaweed <sanga944@naver.com>
2025-06-26 00:31:34 +0900

    script.js

--- script.js
+++ script.js
@@ -106,8 +106,8 @@ function getFormatConfig(format) {
             canvasHeight: 258,
             visualX: 260,
             visualY: 48,
-            visualWidth: 163, // 1:1
-            visualHeight: 163,
+            visualWidth: 164, // 1:1
+            visualHeight: 164,
             borderRadius: 20
         };
     } else if (format === 'mo2') {
